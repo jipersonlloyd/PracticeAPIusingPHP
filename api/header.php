727b22cf77f9bbb5f549a98b094df932ad440f90
Lloyd Jiperson Diaz <lloyddiaz0205@gmail.com>
2024-09-01 23:41:52 +0800

    fixed

--- api/header.php
+++ api/header.php
@@ -4,3 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
 header("Access-Control-Allow-Headers: access, Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
